7cd94306998817571086954d6735c912da0183ce
keke87cc <sky88871111@gmail.com>
2019-05-12 20:42:18 +0800

    ayaya

--- index/js/index.js
+++ index/js/index.js
@@ -1,12 +1,13 @@
 $(function() {
     $('.left a,.right a').on('click', function(e) {
       e.preventDefault();
-      $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left}, 500, 'linear');
+      $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left}, 500, 'swing');
     });
   });
 $(function() {
   $('.up a,.down a').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'swing');
   });
 });
+
