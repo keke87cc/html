be497733100d4b7e03325241be00ad8d7084a258
keke87cc <sky88871111@gmail.com>
2019-06-01 09:58:14 +0800

    yaaa

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-$(function() {
-    $('.left a,.right a').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left}, 500, 'swing');
-    });
-  });
-$(function() {
-  $('.up a,.down a').on('click', function(e) {
+var page = document.getElementById("page");
+var currentpage = 1;
+
+function addpage(n) {
+    currentpage = currentpage + n;
+}
+
+$('.left a,.right a').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'swing');
-  });
+    $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left }, 500, 'swing');
+});
+
+$('.up a,.down a').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'swing');
 });
