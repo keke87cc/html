0f3a1ab399ca52daa74ec446b9a55d7e7a1cbbe3
keke87cc <sky88871111@gmail.com>
2019-06-02 20:29:47 +0800

    ww

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
-var page = document.getElementById("page");
-var currentpage = 1;
-
-function addpage(n) {
-    currentpage = currentpage + n;
-}
-
 $('.left a,.right a').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left }, 500, 'swing');
+    $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left }, 800, 'swing');
 });
 
 $('.up a,.down a').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'swing');
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 800, 'swing');
 });
+
+function openPage(pageName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent3");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(pageName).style.display = "block";
+}
+document.getElementById("defaultOpen").click();
