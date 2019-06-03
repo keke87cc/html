69db85e7425b21f23b7e7e6d35b065ec90bb4197
keke87cc <sky88871111@gmail.com>
2019-06-03 14:50:28 +0800

    adad

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+	$(document).keydown(function(event) {
+	    	    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+    
+		event.preventDefault();
+		// 107 Num Key  +
+		//109 Num Key  -
+		//173 Min Key  hyphen/underscor Hey
+		// 61 Plus key  +/=
+	     }
+	});
+
+	$(window).bind('mousewheel DOMMouseScroll', function (event) {
+	       if (event.ctrlKey == true) {
+     
+		   event.preventDefault();
+	       }
+	});
+});
 $('.up a,.down a').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 800, 'swing');
