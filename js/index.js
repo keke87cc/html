9a4773455292d4e7e47a9b1ec361473954419ec2
keke87cc <sky88871111@gmail.com>
2019-06-03 14:35:11 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-$('.left a,.right a').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollLeft: $($(this).attr('href')).offset().left }, 800, 'swing');
-});
-
 $('.up a,.down a').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 800, 'swing');
