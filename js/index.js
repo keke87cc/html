afe48e1ae9546f4e2826eaf60e7c6a863eb2235c
keke87cc <sky88871111@gmail.com>
2019-06-11 11:21:30 +0800

    Aaa

--- js/index.js
+++ js/index.js
@@ -1,19 +1,8 @@
-// window.onresize = function() {
-//     var zoom = window.devicePixelRatio;
-//     document.body.style.zoom = (100 / zoom) + "%";
-// }
 $('.up a,.down a,button a').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 800, 'swing');
 });
 
-// $(document).ready(function() {
-//     $("#More,#MoreOption").hover(function() {
-//         document.getElementById("MoreOption").style.width = "10%";
-//     }, function() {
-//         document.getElementById("MoreOption").style.width = "0%";
-//     });
-// });
 
 function openPage(Name) {
     $("header").addClass("open");
@@ -33,42 +22,3 @@ function closePage(Name) {
         tab[i].style.display = "none";
     }
 }
-
-// document.addEventListener("keydown", move_man, false);
-
-// function move_man(event) {
-//     var key_event = event.key;
-//     var character = document.getElementById("character");
-//     var posY = 0,
-//         posX = 0;
-//     if (key_event === "ArrowUp") {
-//         character.style.transform = "rotateX(360deg)";
-//         posY = character.offsetTop - 20;
-//         if (posY > 0) {
-//             character.style.top = posY + "px";
-//         }
-//     } else if (key_event === "ArrowDown") {
-//         // character.style.transform = "rotateX(180deg)";
-//         posY = character.offsetTop + 20;
-//         if (posY < document.body.offsetHeight - 200) {
-//             character.style.top = posY + "px";
-//         }
-
-//     } else if (key_event === "ArrowRight") {
-//         // character.style.transform = "rotateZ(90deg)";
-//         posX = character.offsetLeft + 20;
-//         if (posX < document.body.offsetWidth - 120) {
-//             character.style.left = posX + "px";
-//         }
-//         posY = character.offsetTop;
-
-//     } else if (key_event === "ArrowLeft") {
-//         // character.style.transform = "rotateZ(-90deg)";
-//         posX = character.offsetLeft - 20;
-//         if (posX > 0) {
-//             character.style.left = posX + "px";
-//         }
-//         posY = character.offsetTop;
-//     }
-
-// }
